4d4211a58c1dddec309d3430eb5e5aea6aed3138
0xhatsume <0xhatsume@gmail.com>
2024-08-27 01:23:04 +0800

    ✨ Initial vite boiler

---
+++ treasure_island/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {Home} from './pages';
+
+function App() {
+
+  return (
+    <div className="flex flex-col
+    bg-blue-800/20 h-screen
+    ">
+        <Router>
+            <Routes>
+                {/* Lobby Page */}
+                <Route path="/" element={<Home/>} />
+            </Routes>
+        </Router>
+        
+    </div>
+  )
+}
+
+export default App
